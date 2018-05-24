8bb4b5ac1d0b609b85c786981263f6c81dd0f003
Kate Jeziorska <k_jeziorska@yahoo.com>
2018-05-24 12:45:08 -0700

    add business hours plugin

---
+++ plugins/inhabitent_business_hours/views/widget.php
@@ -0,0 +1,32 @@
+<!-- This file is used to markup the public-facing widget. -->
+<?php if(strlen( trim($monday_friday))>0): ?>
+
+<p>
+
+<span><b>Monday-Friday:</b> </span> 
+    <?php echo $monday_friday ?>
+</p>
+
+
+<?php endif?>
+
+<?php if(strlen( trim($saturday))>0): ?>
+<p>
+<span><b>Saturday:</b></span> 
+<?php echo $saturday ?>
+</p>
+
+<?php endif?>
+
+<?php if(strlen( trim($sunday))>0): ?>
+
+<p>
+<span><b>Sunday:</b> </span> 
+<?php echo $sunday ?>
+</p>
+
+<?php endif?>
+
+
+
+
