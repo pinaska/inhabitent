4398360b123f6a24acf0dd9fddbb58b30369c4af
Kate Jeziorska <k_jeziorska@yahoo.com>
2018-05-24 20:39:52 -0700

    style search page by adding a button

--- themes/inhabitent/search.php
+++ themes/inhabitent/search.php
@@ -20,6 +20,7 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'template-parts/content', 'search' ); ?>
+				<a href="<?php echo esc_url(get_permalink())?>" class="button-black-border">Read More &rarr;</a>
 
 			<?php endwhile; ?>
 
