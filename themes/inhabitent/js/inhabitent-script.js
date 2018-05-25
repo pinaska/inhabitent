dd13cafe38478fa34cb0486d3303faaf9127b5fb
Kate Jeziorska <k_jeziorska@yahoo.com>
2018-05-25 11:48:23 -0700

    code the search field  toggle, focus, blur

--- themes/inhabitent/js/inhabitent-script.js
+++ themes/inhabitent/js/inhabitent-script.js
@@ -1,35 +1,24 @@
-// (function($){
-//     //use jquery
+(function ($){
+    // $('body').append('hello');
+    //search-field" for input, search-submit for button
+
+    $('.search-submit').on('click',function (e){
+                e.preventDefault();
+
+                // $('.search-field').hide();
+            
+                $('.search-field').toggleClass('search-field-focus');
+                $('.search-field').focus();
+                $('.search-field').on('blur', function(){
+                    $('.search-field').toggleClass('search-field-focus');
+                }
+            )
+                // focus on the input field 
 
 
+                // have an event for the enter key so we can search e.g. event keycodes check out css trcks for this
 
+                // .search-field .focus 
+    });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// })(jQuery);
+})(jQuery);
