91c602310e92283b11f678738c469b7def0bd28f
Kate Jeziorska <k_jeziorska@yahoo.com>
2018-05-25 14:41:35 -0700

    script and styles for searchform

--- themes/inhabitent/js/inhabitent-script.js
+++ themes/inhabitent/js/inhabitent-script.js
@@ -11,12 +11,21 @@
                 $('.search-field').focus();
                 $('.search-field').on('blur', function(){
                     $('.search-field').toggleClass('search-field-focus');
+                    $('.search-field').val('');
                 }
             )
                 // focus on the input field 
 
 
                 // have an event for the enter key so we can search e.g. event keycodes check out css trcks for this
+            $(document).keypress(function(key){
+                if ( $('.search-field').val().length >0){
+                    if(key.which === 13){
+                        $('.search-form').submit();
+                        return false;
+                    }
+                } 
+            });
 
                 // .search-field .focus 
     });
